9ad82209ff940cf36df145b86e5391e2dcdbe177
saysa <saysaygood83@gmail.com>
2023-09-19 15:30:16 +0100

    allowed the location inout to be put in local storage and made new functions

--- script.js
+++ script.js
@@ -7,18 +7,54 @@ const weatherAPIURL = "https://api.openweathermap.org"
 const weatherAPIKEY = "05c3cef6f340a347a7fc22e0b6607115"
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
+let searchHistory = []
+// ^ the search history is empty at first
+let searchHistoryContainer = $("#history")
+// ^ get the div called history in html
 
+function renderSearchHistory(){
+    searchHistoryContainer.html("");
+                // ?? idk why
 
+    for(let i=0; i< searchHistory.length; i++){
+        let btn = $("<button>");
+        btn.attr("type", "button");
+        btn.addClass("history-btn btn-history")
+
+        btn.text(searchHistory[i])
+        btn.attr("data-search", searchHistory[i]);
+        searchHistoryContainer.append(btn);
+    }
+}
+
+function appendSearchHistory(search){
+    if(searchHistory.indexOf(search) !== -1){
+        // if adding in the same location twice, then it will be returned i.e. not included. if not line 31
+        return
+
+    }
+    searchHistory.push(search);
+    // ^ adds the new location in the array
+    localStorage.setItem("seach-history", JSON.stringify(searchHistory))
+    // makes a new folder in localStorage and allows the search to be more readable in string.
+    renderSearchHistory();
+    
+}
 
 function fetchCoord(search){
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKEY}`;
     // put the whole link incl. your api key in a variable
     // v usual fetch .then stuff
-    fetch(queryURL)
+    fetch(queryURL, {method: "GET"})
         .then(function(data){
             return data.json()
         }).then(function(response){
-            console.log(response);
+            if(!response[0]){
+                // if theres nothing inside the first array of the object, then alert.
+                alert("Location not found")
+            } else{
+                appendSearchHistory(search);
+            }
         })
 }
 
